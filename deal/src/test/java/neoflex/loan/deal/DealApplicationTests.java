1c675b6aecde84063dad3e96d65b2a970f56037e
fedlud <fyodorludinov@gmail.com>
2022-07-15 00:37:30 +0300

    Initial commit

---
+++ deal/src/test/java/neoflex/loan/deal/DealApplicationTests.java
@@ -0,0 +1,13 @@
+package neoflex.loan.deal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
